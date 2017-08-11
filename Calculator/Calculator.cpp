ddc6c48117fba189f4e3b373f32a851040d44472
zhangyabin <zhangyabin@jiandan100.cn>
2017-08-11 11:27:26 +0800

    xiugai

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,16 +1,3 @@
 // Calculator.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-#include "Calculator.h"
-#include <iostream>
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-    Calculator calculator;
-    std::cout << "1 + 2 = " << calculator.add(1, 2) << std::endl;
-    std::cout << "2 - 1 = " << calculator.sub(2, 1) << std::endl;
-
-    return 0;
-}
-
